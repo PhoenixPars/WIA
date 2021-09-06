f591c93153b32726950f73aab79ae65d0ec06f1e
Ashrarhack <60186819+ashrarhacka409@users.noreply.github.com>
2021-09-06 15:30:03 +0430

    Create var.php

---
+++ var.php
@@ -0,0 +1,39 @@
+<?php
+$rhversion = "1.0.0.4";
+$white = "\e[97m";
+$black = "\e[30m\e[1m";
+$yellow = "\e[93m";
+$orange = "\e[38;5;208m";
+$blue   = "\e[34m";
+$lblue  = "\e[36m";
+$cln    = "\e[0m";
+$green  = "\e[92m";
+$fgreen = "\e[32m";
+$red    = "\e[91m";
+$magenta = "\e[35m";
+$bluebg = "\e[44m";
+$lbluebg = "\e[106m";
+$greenbg = "\e[42m";
+$lgreenbg = "\e[102m";
+$yellowbg = "\e[43m";
+$lyellowbg = "\e[103m";
+$redbg = "\e[101m";
+$grey = "\e[37m";
+$cyan = "\e[36m";
+$bold   = "\e[1m";
+function redhawk_banner(){
+  echo "\e[91;1m
+           All In One Tool For\e[36m Information Gathering\e[91m
+                    0
+                   010      01  10  0111110 0110100
+                  01 10     01  01  10   01 1    00
+                 0011100    0111111 01   10 0111101
+                01     10       10  10   01      10
+               10       01      01  0111110 1111101 
+                                                  |0\/0|
+         (CPR) Made by : Ashrarhack A409
+                                                                  \/ \e[97m [$] Shout Out - You ;)
+\e[32m
+  \n";
+}
+?>
